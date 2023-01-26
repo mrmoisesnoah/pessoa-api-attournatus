faf81a677ff5b77632cac26c71678cbbac0d8b95
Moises.Noah <moises.noah@dbccompany.com.br>
2023-01-26 16:22:49 -0300

    Feat: primeiro commit, estrutura da api

---
+++ src/main/java/com/project/pessoaapi/PessoaapiApplication.java
@@ -0,0 +1,13 @@
+package com.project.pessoaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pessoaapi/PessoaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pessoaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
